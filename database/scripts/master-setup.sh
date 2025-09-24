cb6186cc6fbe457fbd491148393e980ccefdf75e
Eliminir <162283888+Eliminir@users.noreply.github.com>
2025-09-24 16:50:34 +0300

    Update master-setup.sh

--- database/scripts/master-setup.sh
+++ database/scripts/master-setup.sh
@@ -1,1 +1,13 @@
 
+#!/bin/bash
+# ... (установка MySQL)
+systemctl restart mysql
+
+# Создание пользователя для репликации
+mysql -e "CREATE USER 'replicator'@'%' IDENTIFIED BY 'strong_password';"
+mysql -e "GRANT REPLICATION SLAVE ON *.* TO 'replicator'@'%';"
+mysql -e "FLUSH PRIVILEGES;"
+
+# Бэкап БД для переноса на Slave
+mysqldump -u root -p wordpress_db > /tmp/wordpress_db.sql
+echo "Скопируйте дамп на Slave сервер и выполните настройку."
